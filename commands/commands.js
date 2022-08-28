a5269733fe294e849be72a9f89402d60b8e8fd08
Thomas <thomas.lefloch@eisti.fr>
2022-08-28 16:57:07 +0200

    feat: adding help menu and fixing some stuff

--- commands/commands.js
+++ commands/commands.js
@@ -1,40 +1,66 @@
 function chinoise(message) {
-    let newWord = "";
-    for (var i = 0; i < message.length; i++) {
-        newWord += ((i % 2 == 0) ? message.charAt(i).toUpperCase() : message.charAt(i).toLowerCase());
-    }
-    return newWord + " 🐍";
+  let newWord = "";
+  for (var i = 0; i < message.length; i++) {
+    newWord +=
+      i % 2 == 0
+        ? message.charAt(i).toUpperCase()
+        : message.charAt(i).toLowerCase();
+  }
+  return newWord + " 🐍";
 }
 
 function snek(channel, messageId) {
-    channel.messages.fetch({ around: messageId, limit: 1 }, { cache: false, force: true }).then(message => {
-        let myMessage = chinoise(message.get(messageId).content);
-        channel.send(myMessage);
-    }).catch(error =>
-        console.log(error)
-    )
+  channel.messages
+    .fetch({ around: messageId, limit: 1 }, { cache: false, force: true })
+    .then((message) => {
+      let myMessage = chinoise(message.get(messageId).content);
+      channel.send(myMessage);
+    })
+    .catch((error) => console.log(error));
 }
 
 function help(channel) {
-    channel.send("Halp me : 🐍");
+  const embed = {
+    title: "Halp 🐍",
+    color: 3066993,
+    description: `Bot qui tsss quand DJULO dit des trucs désagréables`,
+    fields: [
+      {
+        name: "**.tss snek `msgID`**",
+        value:
+          "Envoie un message 'aUtIsTe' 🐍 à partir d'un id de message donné",
+      },
+      {
+        name: "**.tss list**",
+        value: "Affiche la liste des mots désagréables qui déclenchent le bot",
+      },
+    ],
+  };
+  channel.send({
+    embed,
+  });
 }
 
 function addToList(desagreable, word) {
-    if (!desagreable.includes(word)) desagreable.push(desagreable);
+  if (!desagreable.includes(word)) desagreable.push(desagreable);
+}
+
+function removeFromList(desagreable, word) {
+  return desagreable.filter((item) => item !== word);
 }
 
 function list(channel, desagreable) {
-
-    const list = desagreable.join(", ");
-    const embed = {
-        title: "Liste :",
-        color: 3447003, 					//couleur bleue claire
-        description: `${list}`   //ce qui va etre ecrit
-    }
-    channel.send({ embed });
+  const list = desagreable.join(", ");
+  const embed = {
+    title: "Liste :",
+    color: 3066993, //couleur verte
+    description: `${list}`, //ce qui va etre ecrit
+  };
+  channel.send({ embed });
 }
 
-exports.snek = snek
-exports.help = help
-exports.list = list
-exports.addToList = addToList
+exports.snek = snek;
+exports.help = help;
+exports.list = list;
+exports.addToList = addToList;
+exports.removeFromList = removeFromList;
--- index.js
+++ index.js
@@ -1,63 +1,93 @@
-require('dotenv').config();
-const { snek, help, list, addToList } = require('./commands/commands');
-const Discord = require('discord.js');
+require("dotenv").config();
+const {
+  snek,
+  help,
+  list,
+  addToList,
+  removeFromList,
+} = require("./commands/commands");
+const Discord = require("discord.js");
 const myToken = process.env.TOKEN;
 const prefix = ".tss";
 
-const desagreable = ["plutôt", "nuls", "nul", "gueule", "bah", "merde", "tg", "plots", "plot", "foutre"];
+let desagreable = [
+  "nuls",
+  "nul",
+  "gueule",
+  "bah",
+  "merde",
+  "tg",
+  "plots",
+  "plot",
+  "foutre",
+];
+
 const djulo = "248164930652602368";
 
 const client = new Discord.Client();
 
 client.on("ready", () => {
-    console.log("Ready!");
-    client.user.setActivity("Tsss 🐍 | .tss to tsssss...");
+  client.user.setActivity("Tsss 🐍 | .tss to tsssss...");
 });
 
 function parseCommand(message) {
-    const channel = message.channel;
-    const args = message.content.slice(prefix.length).trim().split(/ +/);
-    const command = args.shift().toLowerCase();
-    console.log("Command: " + command);
-    message.delete()
-    switch (command) {
-        case 'snek':
-            snek(channel, args[0]);
-            break;
-        case 'list':
-            list(channel, desagreable);
-            break;
-        case 'addToList':
-            addToList(desagreable, args[0]);
-        default:
-            help(channel);
-            break;
-    }
+  const channel = message.channel;
+  const args = message.content.slice(prefix.length).trim().split(/ +/);
+  const command = args.shift().toLowerCase();
+  try {
+    message.delete();
+  } catch (error) {
+    console.log(error);
+  }
+  switch (command) {
+    case "snek":
+      snek(channel, args[0]);
+      break;
+    case "list":
+      list(channel, desagreable);
+      break;
+    case "addToList":
+      if (message.author.id != djulo) addToList(desagreable, args[0]);
+      break;
+    case "removeFromList":
+      if (message.author.id != djulo) desagreable = removeFromList(desagreable, args[0]);
+      break;
+    default:
+      help(channel);
+      break;
+  }
 }
 
 function isCommand(message) {
-    return (message.content.startsWith(prefix) && message.author.bot === false && message.channel.type !== "dm");
+  return (
+    message.content.startsWith(prefix) &&
+    message.author.bot === false &&
+    message.channel.type !== "dm"
+  );
 }
 
 function tsss(message) {
-    try {
-        message.react("🐍");
-    } catch (error) {
-        message.channel.send("🐍")
-    }
+  try {
+    message.react("🐍");
+  } catch (error) {
+    message.channel.send("🐍");
+  }
 }
 
 function isSnakey(message) {
-    return (message.author.id == djulo && desagreable.some(element => message.content.includes(element)));
+  return (
+    message.author.id == djulo &&
+    desagreable.some((element) => message.content.includes(element))
+  );
 }
 
-client.on('message', (async message => {
-    if (isSnakey(message)) {
-        tsss(message);
-    }
-    if (!isCommand(message)) return
-    parseCommand(message);
-    return
-}));
+client.on("message", async (message) => {
+  if (isSnakey(message)) {
+    tsss(message);
+  }
+  if (!isCommand(message)) return;
+  parseCommand(message);
+  return;
+});
 
-client.login(myToken);    
+client.login(myToken);
